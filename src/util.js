fc82b1e541193428d5502f770a8d67014645b86d
amengdv <aminkafri0082@gmail.com>
2024-12-16 20:55:43 +0800

    remake attempt

--- src/util.js
+++
@@ -1,9 +0,0 @@
-export function getRandomInt(min, max) {
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
-}
-
-export function getRandomBetween(min, max) {
-    return Math.random() * (max - min) + min;
-}
